dab4f344de0066c6a91a98e10b0ed22381fa4f05
paulunder <gameon.padawan@gmail.com>
2025-05-17 09:40:58 +0200

    made build

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pafmzH3v2ld3vWuOX/LEmjQU/Xwx/7jAExqNzRzXoBA=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
